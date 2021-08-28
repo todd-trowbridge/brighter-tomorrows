a404d8d61f7c74969cb61a8f8aa74cbe16b16c35
Todd Trowbridge <84187785+todd-trowbridge@users.noreply.github.com>
2021-08-28 15:57:20 -0400

    production env external https fetch test

--- src/react-redux/Tests/index.js
+++ src/react-redux/Tests/index.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
 function Tests() {
+  const fetchHTTPS = () => {
+    // let url = `www.omdbapi.com/?apikey=${key}=`;
+    let url = "https://jsonplaceholder.typicode.com/todos/"
+    // fetch(encodeURI(url + searchTerm))
+    fetch(encodeURI(url))
+      .then((res) => res.json())
+      .then((data) => {
+        // setMovies(data.Search);
+        console.log(data)
+      });
+  };
   return (
     <div>
       <h1>Tests</h1>
+      {fetchHTTPS()}
     </div>
   )
 }
